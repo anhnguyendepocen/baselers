d60833eb4d9972709bf54133062f1940cbd08339
Nathaniel Phillips <nathaniel.phillips@roche.com>
2018-06-08 15:49:04 +0200

    minor

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,4 @@
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage("baselers v0.1.,. Email: therbootcamp@gmail.com")
-  packageStartupMessage("Type \"?baselers\" for a describtion of the baselers dataset.")
+  packageStartupMessage("baselers v0.1.0. Email: therbootcamp@gmail.com")
+  packageStartupMessage("Type \"?baselers\" for a description of the baselers dataset.")
 }

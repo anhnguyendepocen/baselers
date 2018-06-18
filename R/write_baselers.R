173693adb5eff33492c7bebf0b8863874e19bee9
mdsteiner <markus.steiner@unibas.ch>
2018-06-18 17:31:48 +0200

    added readr that write_csv can be used

--- R/write_baselers.R
+++ R/write_baselers.R
@@ -5,13 +5,14 @@
 #'
 #' @export
 #'
+#' @importFrom readr write_csv
 write_baselers <- function(nsim = 10000, seed = 1) {
 
   baselers <- simulate_baselers(nsim = nsim, seed = seed)
 
   if(file.exists("inst/extdata/baselers.txt")) {file.remove("inst/extdata/baselers.txt")}
 
-  write_csv(x = baselers, path = "inst/extdata/baselers.txt")
+  readr::write_csv(x = baselers, path = "inst/extdata/baselers.txt")
 
   if(file.exists("data/baselers.RData")) {file.remove("data/baselers.RData")}
 
